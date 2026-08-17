b00a77e2686de21533afbd7a1c605c9dcecf1c39
optimisme <optimisme@gmail.com>
2026-08-17 16:18:34 +0200

    .

--- src/config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const os = require('os');
-
-const config = {
-  port: parseInt(process.env.PORT) || 3000,
-  tempDir: process.env.TEMP_DIR || path.join(os.tmpdir(), 'xtec-submissions'),
-};
-
-module.exports = config;
